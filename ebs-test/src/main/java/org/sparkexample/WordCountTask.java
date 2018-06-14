16ef6c37634f683ab205e46256308d2b3b7e48ef
Nikos Gavalas <ns.gavalas@gmail.com>
2018-06-14 15:05:11 +0300

    code cleanup

--- ebs-test/src/main/java/org/sparkexample/WordCountTask.java
+++ ebs-test/src/main/java/org/sparkexample/WordCountTask.java
@@ -50,17 +50,12 @@ public class WordCountTask {
     //esRDD.take(100).forEach(p -> );
 
 
-    //esRDD.collect().forEach(i -> myMap.putAll(i));
     List<Object> rddItems = new ArrayList<>();
     esRDD.collect().forEach(i -> {
       for(Map.Entry<String, Object> miRdd : i.entrySet() ){
-        if(miRdd.getKey().equals("message") && miRdd.getValue() != null && miRdd.getValue().toString().trim().matches(input)){
+        if(miRdd.getKey().equals("message") && miRdd.getValue() != null && miRdd.getValue().toString().matches(input)){
           rddItems.add(miRdd.getValue());
         }
-        else if(miRdd.getKey().equals("message") && miRdd.getValue() != null){
-          System.out.println(miRdd.getValue());
-        }
-
       }
     });
     System.out.println("list size: " + rddItems.size());
@@ -87,54 +82,8 @@ public class WordCountTask {
 
 
 
-   /* List<String> msg = new ArrayList<>();
-    System.out.println("size: " + myMap.size());
-
-    for(Map.Entry<String, Object>  obj :  myMap.entrySet()){
-      if ( obj.getKey().equals("message") ){
-
-        msg.add((String) obj.getValue());
-      }
-    }*/
-
-    // Print the messages
-/*
-    for (String s: msg){
-        System.out.println("message: " + s);
-    //}
-    }
-*/
 
   }
-
-  
-//  public void run(String inputFilePath) {
-//    SparkConf conf = new SparkConf()
-//        .setAppName(WordCountTask.class.getName());
-//    conf.set("spark.es.index.auto.create", "true");
-//    conf.set("spark.es.nodes", "elastest_esnode_1");
-//    JavaSparkContext context = new JavaSparkContext(conf);
-////    JavaPairRDD<String, Map<String, Object>> esRDD =
-////            JavaEsSpark.esRDD(jsc, "radio/artists");
-//
-//    JavaRDD<Map<String, Object>> esRDD = esRDD(context, "362", "?q=me*").values();
-//    esRDD.take(100).forEach(System.out::println);
-//    esRDD.take(100).forEach(p -> System.out.println(p));
-//
-//    context.textFile(inputFilePath)
-//            .flatMap(text -> Arrays.asList(text.split(" ")).iterator())
-//            .mapToPair(word -> new Tuple2<>(word, 1))
-//            .reduceByKey((a, b) -> a + b); //.saveAsTextFile("/out.txt");
-    //JavaRDD<Object> jrdd = context.parallelize(Arrays.asList(context));
-
-
-    //JavaRDD  javaRDD =
- //   JavaEsSpark.saveToEs(jrdd, "spark/testresult");
-//  }
-
-
-
-
 }
 
 
